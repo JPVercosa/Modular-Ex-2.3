79d7365634f94f6dea937cb71fe5412ffe2001db
João Verçosa <jpkqvercosa@hotmail.com>
2020-09-08 09:17:01 -0300

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lista.h"
 
+typedef struct no No;
+
+struct no{
+  void *info;
+  struct no *prox;
+  struct no *ant;
+}; 
+
+struct lista {
+  int tam;
+  No *ini;
+  No *fim;
+  No *corr;
+};
+  

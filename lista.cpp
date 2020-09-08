9f4b61b113dc32a221a40a010ad03996edc75126
João Verçosa <jpkqvercosa@hotmail.com>
2020-09-08 11:28:51 -0300

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -49,33 +49,32 @@ void *lst_retFin(Lista* ls){
 
 void lst_posIni(Lista* ls){
   if (lst_vazia(ls)) {
-		printf("Lista Vazia");
-		ls->corr = NULL;
+    printf("Lista Vazia");
+    ls->corr = NULL;
  } 
   else {
-		No temp = ls->ini;
-		while(temp->prox != NULL) {
-			if (temp->ant==NULL) {
-				ls->corr = temp;
-			}
-			temp=temp->prox;
-		}
+	  No temp = ls->ini;
+	  while(temp->prox != NULL) {
+	    if (temp->ant==NULL) {
+        ls->corr = temp;
+      }
+      temp=temp->prox;
+    }
 }
 
 void lst_posFin(Lista* ls){
   if (lst_vazia(ls)) {
-		printf("Lista Vazia");
+    printf("Lista Vazia");
     ls->corr = NULL;
  } 
   else {
-		No temp = ls->fin;
-		while(temp->ant != NULL) {
-			if (temp->prox==NULL) {
-				ls->corr = temp;
-			}
-			temp=temp->ant;
-		}
-  
+    No temp = ls->fin;
+    while(temp->ant != NULL) {
+      if (temp->prox==NULL) {
+        ls->corr = temp;
+      }
+      temp=temp->ant;
+    }
 }
 
 void *lst_prox(Lista* ls){

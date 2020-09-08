78e99b1023d9680d3ed3d4755dd254c8a60ee4c0
João Verçosa <jpkqvercosa@hotmail.com>
2020-09-08 09:25:37 -0300

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -16,4 +16,58 @@ struct lista {
   No *fim;
   No *corr;
 };
+
+Lista *lst_cria(void elem){
+  //JG
+
+}
+
+int lst_vazia(Lista* ls){
+ //JG
   
+}
+
+void lst_insIni(Lista* ls, void* elem){
+ //Matheus 
+  
+}
+
+void lst_insFin(Lista* ls, void* elem){
+ //Matheus 
+  
+}
+
+void *lst_retIni(Lista* ls){
+ //Matheus 
+  
+}
+
+void *lst_retFin(Lista* ls){
+ //Matheus 
+  
+}
+
+void lst_posIni(Lista* ls){
+ //JP 
+  
+}
+
+void lst_posFin(Lista* ls){
+  //JP
+  
+}
+
+void *lst_prox(Lista* ls){
+  //JG
+  
+}
+
+void *lst_ant(Lista* ls){
+  //JG
+  
+}
+
+void lst_libera(Lista* ls){
+  //JG
+  
+}

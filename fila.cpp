8c0914afe232f97d3f5cd4096fe2627ad0532e9e
João Verçosa <jpkqvercosa@hotmail.com>
2020-09-15 09:45:56 -0300

    Create fila.cpp

---
+++ fila.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lista.h"
+
+struct fila {
+  Lista *lst;
+ };
+ 
+ /*Fila *fila_cria(void elem){
+
+  return 0;
+}*/
+ 
+/*int fila_vazia(Fila *f){
+ 
+  return 0;
+}*/
+ 
+/*void fila_enq(Fila *f, void *elem){
+ 
+  return 0;
+}*/
+  
+/*void *fila_deq(Fila *f){
+
+}*/
+
+/*void fila_livera(fila *f){
+
+}*/
+
+ 
+ 
